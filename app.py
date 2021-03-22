ff3f0f7cacdb0fb7e3a4d8372b2def92c129b02f
VictorWen <42454291+VictorWen@users.noreply.github.com>
2021-03-22 17:38:57 -0400

    Finished Flask Tutorial

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask
+from flask import request
+from flask import redirect
+from flask import render_template
+from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class TodoTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<TodoTask {self.id}>'
+
+
+@app.route('/', methods=['POST','GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form["taskname"]
+        new_task = TodoTask(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an error when adding your task"
+
+    else:
+        tasks = TodoTask.query.order_by(TodoTask.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = TodoTask.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was an error when deleting that task"
+
+
+@app.route('/update/<int:id>', methods=['GET','POST'])
+def update(id):
+    task = TodoTask.query.get_or_404(id)
+
+    if request.method == "POST":
+        task.content = request.form["taskname"]
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an error when updating that task"
+    else:
+        return render_template('update.html',task=task)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
